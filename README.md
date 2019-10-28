0e2f80155a2b84a671f9c1653964d5ae48555c80
Francis F <f.forget@polymtl.ca>
2019-10-28 09:59:54 -0400

    translation.py can now be called from the CMD. Check README!

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-HERCULES-EXTRACTION
+# HERCULES-EXTRACTION
+
+## translation.py
+### Description
+This Python script allows to translate a text file using the Google Translation Cloud API. 
+
+### Usage
+src_text: Path to the text file that needs translation
+
+src_lang: Language of the source text file
+
+dest_text: Path to the translated text file
+
+dest_lang: Desired language for the translated file
+
+### Exemples
+`python3 ./translation.py ./input_en.txt en ./output_fr.txt fr`
--- translation/translation.py
+++ translation/translation.py
@@ -1,33 +1,47 @@
 from googletrans import Translator
 from nltk.tokenize import sent_tokenize, word_tokenize
 import codecs
+import sys
 
-# Initialization
-translator = Translator()
-sentences_per_lines = []
-translated_sentences_per_lines = []
+def read_file(src_text):
+    sentences_per_lines = []
+    text_file = open(src_text, "r")
+    text_lines = text_file.readlines()
 
-# Read the text file
-text_file = open("input_en.txt", "r")
-text_lines = text_file.readlines()
+    for sentences in text_lines:
+        sentences_per_lines.append(sent_tokenize(sentences))
+    return sentences_per_lines
 
-for sentences in text_lines:
-    sentences_per_lines.append(sent_tokenize(sentences))
+def translate_sentences_per_lines(sentences_per_lines, src_lang, dest_lang):
+    translated_sentences_per_lines = []
+    for lines in sentences_per_lines:
+        translated_lines = []
+        for sentences in lines:
+            translated_lines.append(translator.translate(sentences, dest=dest_lang, src=src_lang).text)
+        translated_sentences_per_lines.append(translated_lines)
+    return translated_sentences_per_lines
 
-# Translation
-source_lang = 'en'
-dest_lang = 'fr'
+def export_translated_file(translated_sentences_per_lines, dest_text):
+    translated_file = codecs.open(dest_text, "w", "utf-8") 
 
-for lines in sentences_per_lines:
-    translated_lines = []
-    for sentences in lines:
-        translated_lines.append(translator.translate(sentences, dest=dest_lang, src=source_lang).text)
-    translated_sentences_per_lines.append(translated_lines)
+    for lines in translated_sentences_per_lines:
+        for sentences in lines:
+            translated_file.write(sentences + " ")
+        translated_file.write("\n")
 
-# Output
-translated_file = codecs.open("output_fr.txt", "w", "utf-8") 
+def translate(src_text, src_lang, dest_text, dest_lang):
+    sentences_per_lines = read_file(src_text)
+    translated_sentences_per_lines = translate_sentences_per_lines(sentences_per_lines, src_lang, dest_lang)
+    return export_translated_file(translated_sentences_per_lines, dest_text)
 
-for lines in translated_sentences_per_lines:
-    for sentences in lines:
-        translated_file.write(sentences + " ")
-    translated_file.write("\n")
+if __name__ == "__main__":
+    src_text = sys.argv[1]
+    src_lang = sys.argv[2]
+    dest_text = sys.argv[3]
+    dest_lang = sys.argv[4]
+
+    # Initialization
+    translator = Translator()
+
+    #Translation
+    translate(src_text, src_lang, dest_text, dest_lang)
