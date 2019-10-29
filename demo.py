763d78466ba68d04389d55e5d71ccc2b02c008e0
Vincent Labonte <vincent.labonte31@gmail.com>
2019-10-29 11:30:57 -0400

    Add original text to demo output

--- demo.py
+++ demo.py
@@ -9,7 +9,12 @@ if __name__ == "__main__":
                 'nouvelle église. Le projet de construction fut confié à l\'architecte new-yorkais d\'origine irlandaise James O\'Donnell ' \
                 'à qui on demanda de réaliser la plus vaste et la plus belle église catholique d\'Amérique du Nord. O\'Donnell arriva à ' \
                 'Montréal en octobre 1823 et proposa un projet aux marguilliers qui l\'acceptèrent rapidement.'
-    
+
+    print('Original text:')
+    print(text)
+
+    print()
+
     extractor = GoogleEntityExtractor()
     print('Entity extracted from the original text:')
     entities = extractor.extract_entity(text)
@@ -18,26 +23,26 @@ if __name__ == "__main__":
 
     print()
 
-    print('Translated text (googletrans) :')
+    print('Translated text (googletrans):')
     translated_text_googletrans = Googletrans.translate(text, 'auto', 'en')
     print(translated_text_googletrans)
 
     print()
 
-    print('Translated text (Google Cloud API) :')
+    print('Translated text (Google Cloud API):')
     translated_text_google_cloud_api = GoogleCloudAPI.translate(text, 'fr', 'en')
     print(translated_text_google_cloud_api)
 
     print()
 
-    print('Entity extracted from the translated text (googletrans) :')
+    print('Entity extracted from the translated text (googletrans):')
     entities = extractor.extract_entity(translated_text_googletrans)
     for entity in entities:
         print(entity)
 
     print()
 
-    print('Entity extracted from the translated text (Google Cloud API) :')
+    print('Entity extracted from the translated text (Google Cloud API):')
     entities = extractor.extract_entity(translated_text_google_cloud_api)
     for entity in entities:
         print(entity)
